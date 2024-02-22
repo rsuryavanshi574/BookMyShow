e799ec59c57817d23fe7dfb6e11577e3c91d72df
Rahul Suryavanshi <rsuryavanshi574@gmail.com>
2024-02-22 15:39:44 +0530

    testfile edited

--- src/test/java/com/bookMyShow_App/BookMyShowAppApplicationTests.java
+++ src/test/java/com/bookMyShow_App/BookMyShowAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bookMyShow_App;
+/*package com.bookMyShow_App;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class BookMyShowAppApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}*/
